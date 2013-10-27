7c1fd4c62f3e8d1013de9880c00322327a01a12f
Steven Maude <steve@github.ubuntu>
2013-10-27 15:46:49 +0000

    Minor changes to help strings.

--- cmdline.py
+++ cmdline.py
@@ -6,14 +6,18 @@ def parse_arguments(args=sys.argv[1:]):
     """Parse arguments of script."""
     cmd_description = "Download tracklistings for BBC radio shows.\n\n" \
                       "Saves to a text file, tags audio file or does both.\n" \
-                      "To select output file, directory and filename must " \
-                      "both be specified.\n" \
+                      "To select output file, filename " \
+                      "must both be specified.\n" \
+                      "If directory is not specified, directory is assumed" \
+                      "to be where script is.\n" \
+                      "Filename is the prefix, for someaudio.m4a, use " \
+                      "--filename someaudio\n" \
                       "Otherwise, an attempt will be made to write " \
                       "text file to current directory.\n" \
                       "If choosing 'both', directory and filename should " \
                       "point to audio file.\n" \
                       "In this case, text file will have same name " \
-                      "as audio file, but with .txt."
+                      "as audio file, but with .txt"
 
     # http://stackoverflow.com/questions/7869345/
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter,
@@ -25,13 +29,14 @@ def parse_arguments(args=sys.argv[1:]):
                         default='text', help=action_help)
     parser.add_argument('pid', help="BBC programme id, e.g. b03fnc82")
     parser.add_argument('--directory', help="output directory")
-    parser.add_argument('--filename', help="output filename")
+    parser.add_argument('--filename', help="output filename prefix")
     return parser.parse_args()
 
 
 def main():
     """Check arguments are retrieved."""
     args = parse_arguments()
+    print args
     print "action", args.action
     print "pid", args.pid
     print "directory", args.directory
