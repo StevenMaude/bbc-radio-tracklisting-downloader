87bd53fc08e97914a33a155ce9eeece614204ca7
Steven Maude <steve@github.ubuntu>
2015-02-14 22:42:38 +0000

    It's now 2015, not 2013; bump date

--- bbc_tracklist.py
+++ bbc_tracklist.py
@@ -4,7 +4,7 @@
 # bbc_radio_tracklisting_downloader: Download radio tracklistings from
 # BBC's website and outputs to a text file.
 
-# Copyright 2013 Steven Maude
+# Copyright 2015 Steven Maude
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
